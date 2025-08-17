8e9f79cdfd4f0db3d14c007206ee33a0831b8e73
RoastyToast06 <gomesaiden06@gmail.com>
2025-08-17 19:06:15 -0400

    Refactor game logic and testing showcase. - Reorganize previous code for better readability - Add additional game messages for defeated entities - Fix game over message pinging twice every game - Add new testing video to showcase new features - Update README to reflect new testing video

--- AidenGomes-COIS2020-A2/Game/Game.cs
+++ AidenGomes-COIS2020-A2/Game/Game.cs
@@ -11,8 +11,8 @@ public class WizardFighterDX : Visualizer
     private const int NUM_GOBLINS = 10;
 
     private List<CombatEntity> AllEntities { get; set; }
-    private List<Goblin> Goblins { get; set;}
-    private List<Wizard> Wizards  { get; set;}
+    private List<Goblin> Goblins { get; set; }
+    private List<Wizard> Wizards  { get; set; }
 
 
     public WizardFighterDX()
@@ -32,17 +32,13 @@ public class WizardFighterDX : Visualizer
             AllEntities.Add(new Goblin());
             Goblins.Add((Goblin)AllEntities[^1]);
         }
-        // Sort the list of all entities by their HP from highest to lowest
+
         AllEntities.Sort((a, b) => b.HP.CompareTo(a.HP));
-        LogMessage("Welcome to my game! ~Aiden Gomes~ (0801606)");
+        LogMessage("Welcome to my game! ~Aiden Gomes~");
     }
 
 
-    protected override IEnumerable<CombatEntity> GetEntities()
-    {
-        return AllEntities;
-    }
-
+    protected override IEnumerable<CombatEntity> GetEntities() { return AllEntities; }
 
     protected override void Update()
     {
@@ -54,15 +50,14 @@ public class WizardFighterDX : Visualizer
             float dx = RNG.NextSingle() - 0.5f;
             float dy = RNG.NextSingle() - 0.5f;
             entity.Move(dx, dy);
-
-            // Make sure they don't wiggle off the board
             entity.ClampPosition(EntityXRange, EntityYRange);
 
             //Entities attack if the opponent is in range, but only if it can attack at that frame
             if (entity is Goblin goblin)
             {
-                //Goblins move in random directions at 0.6 units per frame
+                //Goblins move in random directions
                 goblin.Move(RNG.NextSingle() - 0.5f, RNG.NextSingle() - 0.5f);
+
                 //List all wizards in range
                 var wizardsInRange = Wizards.Where(wizard => goblin.DistanceTo(wizard) <= 1 && goblin.CanAttack(CurrentTimestamp)).ToList();
                 if (wizardsInRange.Count > 0)
@@ -70,6 +65,7 @@ public class WizardFighterDX : Visualizer
                     // Attack the weakest wizard in range
                     var weakestWizard = wizardsInRange.OrderBy(wizard => wizard.HP).FirstOrDefault();
                     if (weakestWizard == null) continue;
+                    
                     goblin.MoveTowards(weakestWizard, goblin.DistanceTo(weakestWizard) - 1.0f);
                     goblin.Attack(weakestWizard, goblin.AttackPower, CurrentTimestamp);
                     weakestWizard.PushAwayFrom(goblin, 1.50f);
@@ -78,6 +74,8 @@ public class WizardFighterDX : Visualizer
                     if (weakestWizard.HP <= 0)
                     {
                         deadEntities.Add(weakestWizard);
+                        LogMessage($"{weakestWizard} has been defeated!");
+
                         //Add a new wizard every 50 ticks after one dies
                         if (CurrentTimestamp % 50 == 0)
                         {
@@ -87,14 +85,12 @@ public class WizardFighterDX : Visualizer
                             //Add the wizard to the list of all entities while keeping the list sorted
                             //Find the index of the first wizard with a higher max HP than the new wizard, then insert the new wizard before that index
                             int index = Wizards.FindIndex(w => w.HP < wizard.HP);
-                            if (index == -1)
-                            {
-                                AllEntities.Add(wizard);
-                            }
-                            else
-                            {
-                                AllEntities.Insert(index, wizard);
-                            }
+
+                            if (index == -1) AllEntities.Add(wizard);
+
+                            else AllEntities.Insert(index, wizard);
+
+                            LogMessage($"A new wizard has appeared: {wizard}!");
                         }
                     }
                 }
@@ -103,13 +99,14 @@ public class WizardFighterDX : Visualizer
             {
                 //List all goblins in range
                 var goblinsInRange = Goblins.Where(goblin => wizard.DistanceTo(goblin) <= 1.25 && wizard.CanAttack(CurrentTimestamp)).ToList();
+
                 //If there are goblins in range, attack. Else, move towards the nearest goblin
                 if (goblinsInRange.Count > 0)
                 {
                     // Attack all goblins in range
                     foreach (var gobby in goblinsInRange)
                     {
-                        wizard.Attack(gobby, (int)Math.Ceiling(wizard.SpellLevel/wizard.DistanceTo(gobby)), CurrentTimestamp);
+                        wizard.Attack(gobby, (int)Math.Ceiling(wizard.SpellLevel / wizard.DistanceTo(gobby)), CurrentTimestamp);
                         gobby.PushAwayFrom(wizard, 1.50f);
                         gobby.ClampPosition(EntityXRange, EntityYRange);
 
@@ -117,6 +114,7 @@ public class WizardFighterDX : Visualizer
                         {
                             //Add a new goblin every 15 ticks after one dies
                             deadEntities.Add(gobby);
+                            LogMessage($"{gobby} has been slain by {wizard}");
                             if (CurrentTimestamp % 15 == 0)
                             {
                                 Goblin newGoblin = new();
@@ -125,52 +123,41 @@ public class WizardFighterDX : Visualizer
                                 //Add the goblin to the list of all entities while keeping the list sorted
                                 //Find the index of the first goblin with a higher max HP than the new goblin, then insert the new goblin before that index
                                 int index = Goblins.FindIndex(g => g.HP < newGoblin.HP);
-                                if (index == -1)
-                                    AllEntities.Add(newGoblin);
-                                
-                                else
-                                    AllEntities.Insert(index, newGoblin);
-                            }   
+
+                                if (index == -1) AllEntities.Add(newGoblin);
+
+                                else AllEntities.Insert(index, newGoblin);
+
+                                LogMessage($"A new goblin has appeared: {newGoblin}!");
+                            }
                         }
                     }
                 }
                 else
                 {
                     // Move towards the nearest goblin
-                    //Also check for null in case all goblins are dead
+                    // Also check for null in case all goblins are dead
                     var nearestGoblin = Goblins.OrderBy(goblin => wizard.DistanceTo(goblin)).FirstOrDefault();
                     if (nearestGoblin == null) continue;
-                    
+
                     //Wizards move towards the nearest goblin at 0.5 units per frame
                     wizard.MoveTowards(nearestGoblin, 0.5f);
                 }
             }
-            
+
             //Remove dead entities
             foreach (var deadEntity in deadEntities)
             {
                 AllEntities.Remove(deadEntity);
-                if (deadEntity is Goblin deadGoblin)
-                {
-                    Goblins.Remove(deadGoblin);
-                }
-                else if (deadEntity is Wizard deadWizard)
-                {
-                    Wizards.Remove(deadWizard);
-                }
+                if (deadEntity is Goblin deadGoblin) Goblins.Remove(deadGoblin);
+                else if (deadEntity is Wizard deadWizard) Wizards.Remove(deadWizard);
             }
+        }
 
-            //If all wizards or goblins are dead, end the game
-            if (Wizards.Count == 0 )
-            {
-                IsPlaying = false;
-                LogMessage("All wizards are dead. Goblins win!");
-            }
-            else if (Goblins.Count == 0)
-            {
-                IsPlaying = false;
-                LogMessage("All goblins are dead. Wizards win!");
-            }
+        if (Wizards.Count == 0 || Goblins.Count == 0)
+        {
+            IsPlaying = false;
+            LogMessage($"All {(Wizards.Count > 0 ? "goblins" : "wizards")} are dead. {(Wizards.Count > 0 ? "Wizards" : "Goblins")} win!");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # Wizards vs. Goblins Retro Fighter
+
 This game features an 8-bit wizard fighter against hordes of goblins!
 
 ## Project Details
+
 This game was created as part of a Data Structures & Algorithms assignment using C# & Monogame. My teaching assistant created the renderer & sprite designs for the project; the gameplay logic and sprite implementation are implemented by me. This project utilizes ArrayLists to organize & update entity interactions. Entity logic includes:
+
 - Goblins will attack the wizard with the lowest health; wizards will cast magic to attack all goblins within the spell's range.
 - If there are no goblins in range of a wizard's spell, the wizard will move towards the closest goblin. However, goblins will move in a random direction.
 - During the game, a new goblin will join the battle every 15 frames, whereas new wizards join every 50 frames.
 
 ## Gameplay Video
-https://github.com/user-attachments/assets/00d096b2-d2be-4b8c-8704-1325edc72838
+
+<video controls width="720">
+    <source src="./WizardFighter-Gameplay.mp4" type="video/mp4">
+    Your browser does not support the video tag. 
+    <a href="WizardFighter-Gameplay.mp4"> Download / Play Video</a>
+</video>
+
